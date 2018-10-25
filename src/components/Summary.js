6da4dc2d8ecb6eb4149f8a623ddac09772e064d0
eranbrown92 <eranbrown92@gmail.com>
2018-10-25 16:12:29 -0500

    switched UI

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -6,17 +6,17 @@ class Summary extends React.Component {
   constructor() {
     super();
     this.state = {
-      details: []
+      details: ["one", "two"]
     };
   }
   handleFormData(data) {}
   render() {
     return (
       <div>
+        <CheckoutForm handleFormData={this.handleFormData.bind(this)} />
         {this.state.details.map(single => {
           return <SignedOutGov details={single} />;
         })}
-        <CheckoutForm handleFormData={this.handleFormData.bind(this)} />
       </div>
     );
   }
